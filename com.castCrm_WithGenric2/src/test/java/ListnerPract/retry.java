461d642dc1816ef2bd0972de975821cccb7997de
admin <admin@DESKTOP-HTOPON3>
2024-12-27 10:42:50 +0530

    just for execution remove retry.java from retryTest.java

--- com.castCrm_WithGenric2/src/test/java/ListnerPract/retry.java
+++ com.castCrm_WithGenric2/src/test/java/ListnerPract/retry.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 import testngPractice_base_utility.BaseconfigClass;
 
-public class retry_Test extends BaseconfigClass {
+public class retry extends BaseconfigClass {
 	@Test(retryAnalyzer = com.practice.list_crm_Listnerutility.retryAnalyzerImp.class)
 	public void activesim()
 	{
